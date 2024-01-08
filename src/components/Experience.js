9da8ba7191106e377bbe690abc67a615caf90ad0
shalinijha31 <shalinikumari2k2@gmail.com>
2024-01-08 15:02:24 +0530

    add

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -8,8 +8,6 @@ const Experience = () => {
             </div>
             <div class="border-b border-gray-500 my-1"></div>
             <div >
-
-
                 <div className='flex justify-between '>
                     <ul className=' list-disc flex  pl-5 font-bold '><li>Web Development and Designing Intern at CodeClause. </li></ul>
                     <div>March-2023</div>
@@ -17,7 +15,6 @@ const Experience = () => {
                 <div className=' flex pl-4'> Developed a Portfolio Website , Unit Converter .</div>
             </div>
             <div className=' my-3'>
-
                 <div className='flex justify-between '>
                     <ul className=' list-disc flex  pl-5 font-bold '><li>Web Development and Designing Intern at Oasis Infobyte. </li></ul>
 
